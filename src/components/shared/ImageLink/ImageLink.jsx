5c7854e0f6c4849a2e26256045bc5ff8b354f4f9
EevgenK <45nalitay45@gmail.com>
2024-11-10 12:28:08 +0200

    check links

--- src/components/shared/ImageLink/ImageLink.jsx
+++ src/components/shared/ImageLink/ImageLink.jsx
@@ -1,8 +1,10 @@
+import { FaGithub } from "react-icons/fa6";
 import s from "./ImageLink.module.css";
 
 const ImageLink = ({ data }) => {
   return (
-    <a className={s.link} href="#">
+    <a className={s.link} href="https://github.com/EevgenK">
+      <FaGithub className={s.icon} />
       <img
         srcSet={`${data.img} 1x, ${data.imgX} 2x`}
         src={data.img}
